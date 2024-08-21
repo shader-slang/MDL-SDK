89bc799a0e943f26fd652ae490234ff8c88585d9
Venkataram Sivaram <vedavamadath@nvidia.com>
2024-08-21 14:44:09 -0700

    Compilation scripts

---
+++ examples/mdl_sdk/dxr/content/slangified/compile-ordinary.bat
@@ -0,0 +1,7 @@
+del *.dxil
+del *.slang-module
+slangc .\common.slang -o .\common.slang-module
+slangc .\material.slang -o .\material.slang-module
+slangc .\mdl_renderer_runtime.slang -o .\mdl_renderer_runtime.slang-module
+slangc .\mdl_target_code_types.slang -o .\mdl_target_code_types.slang-module
+slangc.exe .\mdl_hit_programs.slang -profile lib_6_6 -target dxil -o mdl_linked_slang.dxil
