53a25b9965c7666906c31963cfc969c9b09157d2
Venkataram Sivaram <vedavamadath@nvidia.com>
2024-08-22 11:23:09 -0700

    Tweaking compilation script

--- examples/mdl_sdk/dxr/content/slangified/compile-precompiled.bat
+++ examples/mdl_sdk/dxr/content/slangified/compile-precompiled.bat
@@ -1,7 +1,7 @@
 del *.dxil
 del *.slang-module
-slangc .\common.slang -embed-dxil -profile lib_6_6 -o .\common.slang-module
-slangc .\material.slang -embed-dxil -profile lib_6_6 -o .\material.slang-module
-slangc .\mdl_renderer_runtime.slang -embed-dxil -profile lib_6_6 -o .\mdl_renderer_runtime.slang-module
-slangc .\mdl_target_code_types.slang -embed-dxil -profile lib_6_6 -o .\mdl_target_code_types.slang-module
-slangc.exe .\mdl_hit_programs.slang -profile lib_6_6 -target dxil -o mdl_linked_slang.dxil
+slangc .\common.slang -embed-dxil -o .\common.slang-module
+slangc .\material.slang -embed-dxil -o .\material.slang-module
+slangc .\mdl_renderer_runtime.slang -embed-dxil -o .\mdl_renderer_runtime.slang-module
+slangc .\mdl_target_code_types.slang -embed-dxil -o .\mdl_target_code_types.slang-module
+slangc.exe .\mdl_hit_programs.slang -profile lib_6_3 -target dxil -o mdl_linked_slang.dxil
