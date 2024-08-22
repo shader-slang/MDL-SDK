e8e09cd12881801c140a4cc78d580569c3c25008
Venkataram Sivaram <vedavamadath@nvidia.com>
2024-08-22 13:12:29 -0700

    Reverting lib_6_3 workaround

--- examples/mdl_sdk/dxr/content/slangified/compile-ordinary.bat
+++ examples/mdl_sdk/dxr/content/slangified/compile-ordinary.bat
@@ -4,4 +4,4 @@ slangc .\common.slang -o .\common.slang-module
 slangc .\material.slang -o .\material.slang-module
 slangc .\mdl_renderer_runtime.slang -o .\mdl_renderer_runtime.slang-module
 slangc .\mdl_target_code_types.slang -o .\mdl_target_code_types.slang-module
-slangc.exe .\mdl_hit_programs.slang -profile lib_6_3 -target dxil -o mdl_linked_slang.dxil
+slangc.exe .\mdl_hit_programs.slang -profile lib_6_6 -target dxil -o mdl_linked_slang.dxil
