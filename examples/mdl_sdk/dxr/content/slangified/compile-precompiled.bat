b011a3c3ca0cd92b1aa13a9f303120bf28434e8f
Venkataram Sivaram <vedavamadath@nvidia.com>
2024-08-28 11:33:03 -0700

    With perf results

--- examples/mdl_sdk/dxr/content/slangified/compile-precompiled.bat
+++ examples/mdl_sdk/dxr/content/slangified/compile-precompiled.bat
@@ -4,4 +4,4 @@ slangc .\common.slang -embed-dxil -o .\common.slang-module
 slangc .\material.slang -embed-dxil -o .\material.slang-module
 slangc .\mdl_renderer_runtime.slang -embed-dxil -o .\mdl_renderer_runtime.slang-module
 slangc .\mdl_target_code_types.slang -embed-dxil -o .\mdl_target_code_types.slang-module
-slangc.exe .\mdl_hit_programs.slang -profile lib_6_6 -target dxil -o mdl_linked_slang.dxil
+slangc.exe .\mdl_hit_programs.slang -report-perf-benchmark -profile lib_6_6 -target dxil -o mdl_linked_slang.dxil
